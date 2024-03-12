3fddb8baadb9cf92088a6d31ac793e57ada93c07
IK-18 <113296278+IK-18@users.noreply.github.com>
2024-03-12 15:38:23 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./styles/output.css";
+import {MoonPayBuyWidget, MoonPayProvider} from "@moonpay/moonpay-react";
+import Home from "./components/Home";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Converter from "./components/Converter";
+import FiatConverter from "./components/FiatConverter";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<MoonPayProvider
+			apiKey='pk_test_gXuhfbudqsbij76caZNn09gEgakMec'
+			environment='sandbox'
+			debug
+		>
+			<BrowserRouter basename='/'>
+				<Routes>
+					<Route path='/' element={<Home />}></Route>
+				</Routes>
+				<MoonPayBuyWidget
+					variant='overlay'
+					baseCurrencyCode='usd'
+					baseCurrencyAmount='100'
+					defaultCurrencyCode='eth'
+					visible={false}
+				/>
+			</BrowserRouter>
+		</MoonPayProvider>
+	);
 }
 
 export default App;
---
+++ tailwind.config.ts
@@ -0,0 +1,9 @@
+import type {Config} from "tailwindcss";
+const plugin = require("tailwindcss/plugin");
+module.exports = {
+	content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
